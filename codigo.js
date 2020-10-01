b4e10a89d4d713384b17cec5292ca3edcd80b3fa
alanaguiar01 <69945817+alanaguiar01@users.noreply.github.com>
2020-10-01 20:18:54 -0300

    Add files via upload
    Site Para Avaliação desenvolvimento WEB

---
+++ codigo.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="author" content="Alan Martins">
+    <meta name="keywords" content="aula ono">
+    <title>Tudo sobre Som</title>
+  </head>
+  <body>
+    <center>
+      <p style="color: yellow;background : black; font-size: 45px;"> O que
+        preciso saber para montar um som automotivo? </p>
+      <h1 style="border : 2px solid black;"> <i>Tipos de som</i> </h1>
+    </center>
+    
+  </body>
+</html>
